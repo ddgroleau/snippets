54a514528fe8c1aff129cd7746563d39ed288bfe
ACT\groleaud <daniel.groleau@tivityhealth.com>
2021-12-03 11:46:48 -0700

    Updated db-sync.php to support migrations from local to dev environment.

--- db-sync.php
+++ db-sync.php
@@ -1,6 +1,6 @@
 <!-- <?php
 /*
-This is a CLI tool for synchronizing a wordpress database across different environments. This process takes approximately 5-10 minutes to complete.
+This is a CLI tool for synchronizing the wholehealthliving database across different environments. This process takes approximately 5-10 minutes to complete.
 
   Prerequisites:
     1. Current version of PHP is installed.
@@ -9,6 +9,8 @@ This is a CLI tool for synchronizing a wordpress database across different envir
     4. 'mysql' is added to $PATH (i.e. export PATH=$PATH:"/C/Program Files/MySQL/MySQL Workbench 8.0")
 
   To Synchronize Database Environments:
+    * Environment synchronization should follow a linear pattern: local -> dev -> qa -> uat -> prod
+      (You can start anywhere but do not skip a successive environment, i.e. do not go from dev to prod)
     * In the terminal, navigate to the directory where this file exists.
     * Run the command 'php db-sync.php <origin> <destination>'
         where <origin> is the source DB and <destination> is the target DB.
@@ -19,7 +21,6 @@ date_default_timezone_set('America/Phoenix');
 error_reporting(E_ALL);
 
 $environments = json_decode(file_get_contents("db_mask.json"), true);
-
 $arguments = $_SERVER['argv'];
 
 if (count($arguments) != 3) {
@@ -49,53 +50,63 @@ function commence_sync($environments, $arguments) {
   // Extract settings as variables prefixed with origin or destination respectively
   extract($origin, EXTR_PREFIX_ALL, 'origin');
   extract($destination, EXTR_PREFIX_ALL, 'destination');
-
+  
   // Establish patterns with correctly escaped characters for 'replace_hostname'
   $origin_pattern = preg_quote($origin_pattern);
   $destination_pattern = preg_quote($destination_pattern);
   
   // Create migrations directory if it does not exist
-  $migration_dir = "migrations"."\\".date("Y-m-d");
-
+  $migration_dir = "migrations"."\/".date("Y-m-d");
   if ( !is_dir( $migration_dir ) ) {
-    console_log("Creating backup directory at ".__DIR__."\\".$migration_dir.".");
+    console_log("Creating backup directory at ".__DIR__."\/".$migration_dir.".");
     mkdir( $migration_dir );       
   }
-
+  
+  // Instantiate bash command strings
   $filename = "migration-" . date("Y-m-d-H-i-s") . ".sql";
-
-  $backup_destination = "mysqldump -h $destination_db_host -u $destination_db_user  --password=$destination_db_pass --skip-extended-insert --column-statistics=0 $destination_db_name >  $migration_dir/$destination_db_user-$filename";
-  $dump_origin =        "mysqldump -h $origin_db_host      -u $origin_db_user       --password=$origin_db_pass      --skip-extended-insert --column-statistics=0 $origin_db_name      >  $migration_dir/$origin_db_user-$filename" ;
-  $replace_hostname =   "sed 's/$origin_pattern/$destination_pattern/g'  $migration_dir/$origin_db_user-$filename >  $migration_dir/From$origin_db_user-To$destination_db_user-$filename";
-  $import_environment = "mysql     -h $destination_db_host  -u $destination_db_user --password=$destination_db_pass $destination_db_name <  $migration_dir/From$origin_db_user-To$destination_db_user-$filename";
-
-  console_log("Backing up destination database...\n".$destination_db_name." ==>  $migration_dir/$destination_db_user-$filename");
+  $backup_destination = "mysqldump -h $destination_db_host -u $destination_db_user  --password=$destination_db_pass --skip-extended-insert --column-statistics=0 $destination_db_name >  $migration_dir/$destination_name-$filename";
+  $dump_origin =        "mysqldump -h $origin_db_host      -u $origin_db_user       --password=$origin_db_pass      --skip-extended-insert --column-statistics=0 $origin_db_name      >  $migration_dir/$origin_name-$filename" ;
+  $replace_hostname =   "sed 's/$origin_pattern/$destination_pattern/g'  $migration_dir/$origin_name-$filename >  $migration_dir/from-{$origin_name}_to-{$destination_name}_$filename";
+  $replace_hostname_with_prefix =  "sed 's/http\:\/\/$origin_pattern/https\:\/\/$destination_pattern/g'   $migration_dir/$origin_name-$filename >  $migration_dir/from-{$origin_name}_to-{$destination_name}_$filename";
+  $import_environment = "mysql -h $destination_db_host  -u $destination_db_user --password=$destination_db_pass $destination_db_name <  $migration_dir/from-{$origin_name}_to-{$destination_name}_$filename";
+  
+  // Backup destination db
+  console_log("Backing up destination database...\n".$destination_db_name." ==>  $migration_dir/$destination_name-$filename");
   passthru($backup_destination);
 
-  console_log("Dumping contents of origin database...\n".$environments[$arguments[0]]['wp_home']." ==>  $migration_dir/$origin_db_user-$filename");
+  // Get contents of origin db
+  console_log("Dumping contents of origin database...\n".$environments[$arguments[0]]['wp_home']." ==>  $migration_dir/$origin_name-$filename");
   passthru($dump_origin);
 
+  // Replace instances of origin hostname with destination hostname in sql script
   console_log("Finding all occurences of origin hostname within database and replacing with destination hostname...\n $origin_pattern ==> $destination_pattern");
-  passthru($replace_hostname);
+  if($origin_name == "local") {
+    // If migrating from localhost, change 'http' to 'https'
+    passthru($replace_hostname_with_prefix);
+  } else {
+    passthru($replace_hostname);
+  }
 
-  console_log("Importing origin into destination database...\n $migration_dir/$destination_db_user-$filename ==> $destination_db_name");
-  passthru($import_environment);
+  // Confirm import to database
+  console_log("Importing the origin database into the destination database...\n $migration_dir/from-{$origin_name}_to-{$destination_name}_$filename ==> $destination_db_name");
+  $import_acknowledgement = readline("The next step will import the origin database into the destination database. Continue? [y]es [n]o \n");
+  $possible_yes = ['yes','Yes','y','Y'];
 
-  console_log("Synchronization complete. Database updated.");
+  if (in_array($import_acknowledgement, $possible_yes)) {
+    passthru($import_environment);
+  } else {
+    console_log("Migration scripts have been created. Exiting prior to import process execution.");
+    exit(0);
+  }
+    console_log("Synchronization complete. Database updated.");
 }
 
-console_log("Attempting to synchronize ".$arguments[0]." environment (source) with ".$arguments[1]." environment (target)...\n".$environments[$arguments[0]]['wp_home']." ==> ".$environments[$arguments[1]]['wp_home']);
-$line = readline("Continue? [y]es [n]o \n");
-
-$possible_yes = [
-  'yes',
-  'Yes',
-  'y',
-  'Y'
-];
-
 // Confirm sync execution
-if (in_array($line, $possible_yes))
+console_log("Attempting to synchronize ".$arguments[0]." environment (source) with ".$arguments[1]." environment (target)...\n".$environments[$arguments[0]]['wp_home']." ==> ".$environments[$arguments[1]]['wp_home']);
+$synchronize_acknowledgement = readline("Continue? [y]es [n]o \n");
+$possible_yes = ['yes','Yes','y','Y'];
+
+if (in_array($synchronize_acknowledgement, $possible_yes))
   commence_sync($environments, $arguments);
 
 
